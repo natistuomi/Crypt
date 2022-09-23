eedb9e637c1e48fac617b0e0acb29cbe8aec33ba
natistuomi <77719270+natistuomi@users.noreply.github.com>
2022-09-23 13:21:06 +0200

    gui?

---
+++ src/CryptView.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class CryptView {
+    private JTextField messageNameTextField;
+    private JTextField keyNameTextField;
+    private JButton sendButton;
+    private JTextArea cryptTextArea;
+    private JTextArea messageTextArea;
+}
