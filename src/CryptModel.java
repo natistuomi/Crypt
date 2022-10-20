f19584286f7d849fdeadc0754d7ba9b108ee7ad5
natistuomi <77719270+natistuomi@users.noreply.github.com>
2022-10-20 13:24:07 +0200

    kinda works?...

--- src/CryptModel.java
+++ src/CryptModel.java
@@ -5,6 +5,9 @@ public class CryptModel {
     private String encryption;
     private String message;
     private String key;
+    private String fileortext;
+
+
 
     public void setMessage(String message) {
         this.message = message;
@@ -18,60 +21,50 @@ public class CryptModel {
         return encryption;
     }
 
-    public String getMessage() { return message;}
 
-    public String getKey() { return key;}
 
     public void makeEncryption(){
-        String fileortext = makeIsItFileOrText(message, key);
-        message = makeString(message, fileortext);
-        key = makeString(key, fileortext);
+        fileortext = makeIsItFileOrText();
+        message = makeString(message);
+        key = makeString(key);
         encryption = encryptString();
         writeEncryptedFile(encryption);
     }
 
-    public String makeString(String s, String ft){
-        int x = 0;
-        if(ft.length() != 2){
-            x = 1;
-        }
-        return makeMessageOrKeyString(s, ft, x);
+
+
+    public String makeIsItFileOrText(){
+        return isItFileOrString(message) + isItFileOrString(key);
     }
 
-    public String makeMessageOrKeyString(String s, String ft, int x){
-        ft = ft + ' ';
-        if(ft.charAt(x) == 'f'){
+    public String isItFileOrString(String s){
+        String goal = "txt.";
+        int length = s.length();
+        if(length > 4){
+            for(int i = 1; i < 5; i++){
+                if(s.charAt(length-i) != goal.charAt(i-1)){
+                    return "s";
+                }
+            }
+            return "f";
+        }
+        return "s";
+    }
+
+
+
+    public String makeString(String s){
+        int n = 0;
+        if(fileortext.length() != 2){
+            n = 1;
+        }
+        return makeMessageOrKeyString(s, n);
+    }
+
+    public String makeMessageOrKeyString(String s, int n){
+        fileortext = fileortext + ' ';
+        if(fileortext.charAt(n) == 'f'){
             s = readFile(s);
-        }
-        return s;
-    }
-
-    public void writeEncryptedFile(String c){
-        PrintWriter outfile = openTextFileForWriting("encryption.txt");
-        outfile.println(c);
-    }
-
-    public PrintWriter openTextFileForWriting(String filename) {
-        PrintWriter outfile = null;
-        try {
-            outfile = new PrintWriter(new BufferedWriter(new FileWriter(filename)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return outfile;
-    }
-
-    public int cryptChar(char m, char k) {
-        return m^k;
-    }
-
-    public String encryptString(){
-        int kl = key.length();
-        String s = "";
-        for(int i = 0; i < message.length(); i++){
-            char x = message.charAt(i);
-            char y = key.charAt(i%kl);
-            s = s + (char)cryptChar(x,y);
         }
         return s;
     }
@@ -88,8 +81,7 @@ public class CryptModel {
         try {
             in = new BufferedReader(new FileReader(fs));
         } catch (FileNotFoundException e) {
-            System.out.println("File not found, use keyboard instead!");
-            in = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Error with file!");
         }
         return in;
     }
@@ -109,23 +101,39 @@ public class CryptModel {
         return text;
     }
 
-    public String isItFileOrString(String x){
-        String goal = "txt.";
-        int length = x.length();
-        if(length > 4){
-            for(int i = 1; i < 5; i++){
-                if(x.charAt(length-i) != goal.charAt(i-1)){
-                    return "s";
-                }
-            }
-            return "f";
+
+
+    public String encryptString(){
+        int kl = key.length();
+        String s = "";
+        for(int i = 0; i < message.length(); i++){
+            char x = message.charAt(i);
+            char y = key.charAt(i%kl);
+            s = s + (char)cryptChar(x,y);
         }
-        return "s";
+        return s;
     }
 
-    public String makeIsItFileOrText(String m, String k){
-        return isItFileOrString(m) + isItFileOrString(k);
+    public int cryptChar(char m, char k) {
+        return m^k;
     }
 
+
+
+    public void writeEncryptedFile(String s){
+        PrintWriter outfile = openTextFileForWriting("encryption.txt");
+        outfile.print(s);
+        outfile.flush();
+    }
+
+    public PrintWriter openTextFileForWriting(String filename) {
+        PrintWriter outfile = null;
+        try {
+            outfile = new PrintWriter(new BufferedWriter(new FileWriter(filename)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return outfile;
+    }
 }
 
